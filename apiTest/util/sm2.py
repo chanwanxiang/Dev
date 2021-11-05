f40ce3f648a10321c5fb5c8289e6995a910d01d9
wxchan <595366700@qq.com>
2021-11-05 18:15:44 +0800

    fix:apiTest

--- apiTest/util/sm2.py
+++ apiTest/util/sm2.py
@@ -1,27 +1,30 @@
-# -*- coding: utf-8 -*-
-
-import base64
+from base64 import b64encode, b64decode
+import json, requests
 from gmssl import sm2
 
-pubKey = "+0463f587e346663a7bafd85a41e8ade713bd3c3a2168b4891671fcd3b2ea3686f5fb0847a113b1931a630157d5ba335eb173b805f378655ca1c440138390972786"
-priKey = "5fd0febfd7ce7546bc23bce5fe0e6b25df1490d9fe7bd84f479a182272906495"
-sm2Crypt = sm2.CryptSM2(priKey, pubKey)
+resp = json.loads(requests.get("https://gzdev.yuuwei.com/user/getSm2").text)
+pubKey = resp['data']
+
+priKey = "311861d5046d647c698930992b7d2614956d267f46419223714913cf5e0a7150"
+sm2Crypt = sm2.CryptSM2(public_key=pubKey, private_key=priKey)
 
 class Sm2():
     def encryptinfo(self, info):
         """
         将utf-8编码的info信息编码为unicode -> sm2加密 -> 加密密文解码
         """
-        sm2info = sm2Crypt.encrypt(info.encode('utf-8'))
-        b64info = base64.b64encode(sm2info).decode()
+        sm2info = sm2Crypt.encrypt(info.encode(encoding="utf-8"))
+        b64info = b64encode(sm2info).decode()
         return b64info
 
     def decryptinfo(self, info):
-        b64info = base64.b64decode(info.encode())
-        sm2info = sm2Crypt.decrypt(b64info).decode(encoding="utf-8")
-        return sm2info
+        b64info = b64decode(info.encode())
+        deinfo = sm2Crypt.decrypt(b64info).decode("utf8", "ignore")
+        return deinfo
 
 sm2Obj = Sm2()
-info = '123456'
-print(sm2Obj.encryptinfo(info))
-print(sm2Obj.decryptinfo(sm2Obj.encryptinfo(info)))
+pswd = 'Ab@123456'
+enpswd = sm2Obj.encryptinfo(pswd)
+print(enpswd, len(enpswd))
+depswd = sm2Obj.decryptinfo(enpswd)
+print(depswd)
